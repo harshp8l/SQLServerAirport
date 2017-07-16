04da1cdc49f9f2b26598ebe5f9a3600d2136b413
r3dang <>
2017-07-15 19:27:14 -0700

    changed name of flat table to 'sd airport flat table'

--- Tables/dbo.sd_airport_flat_table.sql
+++ Tables/dbo.sd_airport_flat_table.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
-CREATE TABLE [dbo].[sd_airport_flat_table - flat_table] (
+CREATE TABLE [dbo].[sd_airport_flat_table] (
 		[year]                   [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[flight_num]             [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[airline_carrier]        [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -19,5 +19,5 @@ CREATE TABLE [dbo].[sd_airport_flat_table - flat_table] (
 		[material]               [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[sd_airport_flat_table - flat_table] SET (LOCK_ESCALATION = TABLE)
+ALTER TABLE [dbo].[sd_airport_flat_table] SET (LOCK_ESCALATION = TABLE)
 GO
