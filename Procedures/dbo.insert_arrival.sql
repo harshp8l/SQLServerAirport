9ceef3d67078a42d49808cdf9a32c2836012fa27
harshp8l <>
2017-07-16 15:21:01 -0700

    fixed time types

--- Procedures/dbo.insert_arrival.sql
+++ Procedures/dbo.insert_arrival.sql
@@ -2,13 +2,13 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE PROCEDURE [dbo].[insert_arrival]
-	@arrival_time numeric(18,0),
+	@arrival_time time(7),
 	@arrival_city varchar(50),
 	@flight_num numeric(18,0)
 as
 begin
-	begin try
-		begin tran
+	--begin try
+		--begin tran
 		
 		declare @id int
 		exec sp_get_next_id 'Arrival', @id output
@@ -29,14 +29,14 @@ begin
 				@id
 		end
 
-		commit
-	end try
-	begin catch
+		--commit
+	--end try
+	--begin catch
 		--IF @@TRANCOUNT > 0
 		--	rollback
 		--declare @ErrorMessage varchar(200)
 		--select @ErrorMessage = 'value not found in table'
 		--RAISERROR (@ErrorMessage, 16, 1);
-	end catch
+	--end catch
 end
 GO
